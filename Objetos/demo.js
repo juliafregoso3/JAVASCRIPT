fce8b5d835d8bd2ecb400416b89839eff38d03e1
Julia Fregoso Urrutia <juliafregoso3@gmail.com>
2023-04-17 22:34:15 -0600

    Ejercicio objetos - free code camp

---
+++ Objetos/demo.js
@@ -0,0 +1,14 @@
+//Probando con el ejercicio Add New Properties to a JavaScript Object de curso Javascript de Free code camp
+
+const myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+};
+
+console.log(myDog);
+
+myDog.bark = "woof"; //agregamos una propiedad que no existía al objeto myDog.
+
+console.log(myDog);

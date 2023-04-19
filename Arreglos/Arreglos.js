9dd5ff01aad6797bd82ef69866cdf8e42098d934
Julia Fregoso Urrutia <juliafregoso3@gmail.com>
2023-04-18 22:36:23 -0600

    Iterando en arreglos

---
+++ Arreglos/Arreglos.js
@@ -0,0 +1,15 @@
+//? Curso Javascript básico de FreeCodeCamp
+// ? Itera a través de un arreglo con un bucle "for"
+
+//*Declara e inicializa una variable total a 0. Usa un bucle for para sumar el valor de cada elemento del arreglo myArr al total.
+
+// Configuración
+const myArr = [2, 3, 4, 5, 6];
+
+// Cambia solo el código debajo de esta línea
+
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+  total = total + myArr[i];
+}
